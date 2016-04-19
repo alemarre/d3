305f396ca76dc6629328c4cacdcad7968163446b
Armand LEMARRE <Armand.LEMARRE_ssii@i-bp.fr>
2016-04-19 11:24:00 +0200

    Fix #2601

--- src/time/day.js
+++ src/time/day.js
@@ -3,7 +3,7 @@ import "time";
 import "year";
 
 d3_time.day = d3_time_interval(function(date) {
-  var day = new d3_date(2000, 0);
+  var day = new d3_date(2000, 0, 0);
   day.setFullYear(date.getFullYear(), date.getMonth(), date.getDate());
   return day;
 }, function(date, offset) {
